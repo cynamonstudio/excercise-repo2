4f59670cf0b194de30dc59955d60aa0b36dadf14
cynamonstudio <cynamonstudio@gmail.com>
2022-11-01 22:00:39 +0100

    SOME CHNAGES

--- app.js
+++ app.js
@@ -15,3 +15,20 @@ newElement.style.top="0px"
 newElement.style.left="0px"
 newElement.src="cloud.jpg"
 document.body.appendChild(newElement)
+
+
+
+const array = ["hello","world"]
+let count = 0 
+setInterval ( ()=>{
+    console.log("click")
+
+    if ( count==0){
+        document.getElementById("text").innerHTML=array[0]
+        count++
+    }else{
+        document.getElementById("text").innerHTML=array[1]
+        count--
+    }
+
+},1000)
